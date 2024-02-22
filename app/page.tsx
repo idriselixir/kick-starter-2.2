72ee3c42cee43feb1e27fc4e48b52f4a7edf76a4
Idris Noor <idreesnoor150@gmail.com>
2024-02-22 22:32:20 +0300

    updated frontend

---
+++ app/page.tsx
@@ -0,0 +1,25 @@
+'use client'
+import { Sepolia } from "@thirdweb-dev/chains";
+import { ThirdwebProvider, metamaskWallet, coinbaseWallet } from "@thirdweb-dev/react";
+import PlayNow from "@/components/PlayNow";
+import GameOn from "@/components/GameOn";
+import Footer from "@/components/Footer";
+
+
+
+export default function Home() {
+  return (
+    <ThirdwebProvider supportedWallets={[metamaskWallet(), coinbaseWallet()]} clientId="4982c0edd5cdb1f6ac928f6a9fd7c895" activeChain={Sepolia}>
+      <section className="bg-hero h-screen bg-no-repeat bg-cover bg-center">
+        <PlayNow />
+
+      </section>
+      <section>
+        <GameOn />
+      </section>
+
+      <Footer />
+    </ThirdwebProvider>
+
+  );
+}
